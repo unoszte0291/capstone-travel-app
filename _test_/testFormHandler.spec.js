dd384105fdc1eceecc888223836e6a2d2e4340cb
YOSHIDA Tomonari <48991222+unoszte0291@users.noreply.github.com>
2021-04-17 20:22:51 +0900

    Delete testFormHandler.spec.js

--- _test_/testFormHandler.spec.js
+++
@@ -1,7 +0,0 @@
-import {handleSubmit} from '../src/client/js/formHandler';
-
-explanation("Testing the submit", ()=>{
-    test("Testing the handleSubmit()",()=>{
-        expect(handleSubmit).toBeDefined();
-    })
-})
